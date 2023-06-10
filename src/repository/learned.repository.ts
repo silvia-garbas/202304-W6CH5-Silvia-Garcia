56d0b30464d950fd58a5f885339db49e015dbe06
Silvia <silvia.garbas@gmail.com>
2023-06-10 17:38:28 +0200

    Added getById method

--- src/repository/learned.repository.ts
+++ src/repository/learned.repository.ts
@@ -1,6 +1,6 @@
 import fs from 'fs/promises';
 import createDebug from 'debug';
-const debug = createDebug('W6:SampleRepo');
+const debug = createDebug('W6:LearnedRepo');
 
 type Learned = {
   id: string;
@@ -11,11 +11,19 @@ const file = './data.json';
 
 export class LearnedRepo {
   constructor() {
-    debug('Sample Repo');
+    debug('Learned Repo');
   }
 
   async readAll() {
     const stringData = await fs.readFile(file, { encoding: 'utf-8' });
     return JSON.parse(stringData) as Learned[];
   }
+
+async getById(id: string) {
+    const stringData = await fs.readFile(file, { encoding: 'utf-8' });
+    const cucumberData = JSON.parse(stringData) as Learned[];
+    return cucumberData.find((value) => value.id === id);
 }
+}
+
+
